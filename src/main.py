398d5b4bff65fa41598b4517f5d864119f4b3ca7
Olga <o.pogibelnaya@g.nsu.ru>
2024-10-21 14:24:38 +0700

    переход на FastAPI идет полным ходом

--- src/main.py
+++ src/main.py
@@ -1,56 +1,16 @@
-import uvicorn
-import argparse
+import asyncio
 from node import Node
-from fastapi import FastAPI
-from types_of_rpc import RequestVote, RequestVoteResponse
-import httpx
 
+async def start_nodes():
+    node1 = Node(5000, ["localhost:5001", "localhost:5002"])
+    node2 = Node(5001, ["localhost:5000", "localhost:5002"])
+    node3 = Node(5002, ["localhost:5000", "localhost:5001"])
 
-app = FastAPI()
+    await asyncio.gather(
+        node1.start(),
+        node2.start(),
+        node3.start()
+    )
 
-
-# Информация о текущей ноде
-node_id = "localhost:5000"  
-peers = ["localhost:5001", "localhost:5002"]
-
-@app.get("/")
-async def root():
-    return {"message": RequestVote(term=1, candidate_id='1', last_log_index=0, last_log_term=0)}
-
-@app.post("/request-vote", response_model=RequestVoteResponse)
-async def request_vote(request: RequestVote):
-    print(f"Получен запрос голосования от {request.candidate_id} на ноде {node_id}")
-    # Пример обработки запроса голосования
-    vote_granted = True  # Здесь ваша логика голосования
-    return RequestVoteResponse(term=request.term, vote_granted=vote_granted)
-
-# @app.post("/send-message")
-# async def send_message():
-#     async with httpx.AsyncClient() as client:
-#         for peer in peers:
-#             try:
-#                 response = await client.post(f"http://{peer}/request-vote", json={
-#                     "term": 1,
-#                     "candidate_id": node_id,
-#                     "last_log_index": 0,
-#                     "last_log_term": 0
-#                 })
-#                 data = response.json()
-#                 print(f"Ответ от {peer}: {data}")
-#             except Exception as e:
-#                 print(f"Ошибка при подключении к ноде {peer}: {e}")
-
-#     return {"status": "Сообщения отправлены"}
-
-def start_node(host: str, port: int, peers: list[str]):
-    app.state.node_id = f"{host}:{port}"
-    app.state.peers = peers
-    uvicorn.run(app, host=host, port=port)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Запуск P2P ноды Raft")
-    parser.add_argument('--host', type=str, default="127.0.0.1", help="Хост для запуска ноды")
-    parser.add_argument('--port', type=int, required=True, help="Порт для запуска ноды")
-    parser.add_argument('--peers', nargs='+', help="Список других нод (localhost:5001 localhost:5002 и т.д.)")
-    args = parser.parse_args()
-    start_node(args.host, args.port, args.peers)
+if __name__ == "__main__":
+    asyncio.run(start_nodes())
--- src/node.py
+++ src/node.py
@@ -1,18 +1,25 @@
-import threading
+from email import message
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 import asyncio
-import pickle
+import httpx
+from contextlib import asynccontextmanager
+
 from node_state import NodeState
 from types_of_rpc import RequestVote, RequestVoteResponse
 from my_timer import MyTimer
 
+
+
 class Node:
-    def __init__(self, host: str, port: int, list_ip: list[str]):
+    def __init__(self, port: int, nodes: list[str], host: str = "localhost"):
         self.host = host
         self.port = port
         self.name = 'http://' + host + ':' + str(port)
-        self.app = Flask(self.name) # Основной класс приложения Flask. Он управляет маршрутизацией, настройками и запускает веб-сервер.
-        self.start()
-        self.nodes_sessions = {}
+        self.nodes = nodes
+        self.app = FastAPI(lifespan=self.lifespan)
+        
         self.state = NodeState.FOLLOWER
         self.current_term = 0
         self.voted_for = None
@@ -21,101 +28,59 @@ class Node:
         self.commit_index = 0
         self.last_applied = 0
         self.last_log_term = 0
-        for key in ['http://' + ip for ip in list_ip]:
-            self.nodes_sessions[key] = None
         self.next_index = {}
         self.match_index = {}
-        self.refresh_next_match_index()
-        self.election_timer = MyTimer(self.start_election)
-        self.election_timer.start()
+        # self.refresh_next_match_index()
+        # self.election_timer = MyTimer(self.start_election)
+        # self.election_timer.start()
+               
+        @self.app.get("/")
+        async def hello_from_node():
+            return {"message": f"Hello from Node at {self.host}:{self.port}"}
+
+        @self.app.get("/request-vote")
+        async def send_request_vote():
+            await self.send_parallel_messages(self.nodes, RequestVote(term=1, candidate_id=self.name, last_log_index=0, last_log_term=0))
+            return {"message": "отправил"}
         
-        # Обработчик для входящих сообщений
-        # TODO: доделать
-        @self.app.route('/', methods=['POST'])
-        def receive_message():
-            print("HERERERERER")
-            asyncio.get_running_loop()
-            print("HERE IT IS")
-            data = pickle.loads(request.get_data())
-            print(f"{self.name} получил сообщение: {data}")
-            if isinstance(data, RequestVote): # если пришел запрос RequestVote
-                self.respond_to_RequestVote(data)
-            if isinstance(data, RequestVoteResponse) & self.state == NodeState.CANDIDATE:
-                self.receive_RequestVoteResponse(data)
-            return jsonify({"status": "received"}), 200 # response (jsonify): Объект ответа, который возвращается клиенту.
+        @self.app.post("/request-vote")
+        async def receive_request_vote(vote: RequestVote):
+            print(f"Node {self.host}:{self.port} received a vote request: {vote}")
+            return {"message": f"Node {self.host}:{self.port} received a vote request", "vote_data": vote.model_dump()}
 
-    def start(self):
-        # Запускаем Flask сервер в отдельном потоке
-        thread = threading.Thread(target=self.run_server)
-        thread.daemon = True  # Сделаем поток демоном, чтобы он завершился при остановке программы
-        thread.start()
+        # @self.app.post("/request-vote", response_model=RequestVoteResponse)
+        # async def request_vote(request: RequestVote):
+        #     print(f"Получен запрос голосования от {request.candidate_id} на ноде {app.state.node.name}")
+        #     # Пример обработки запроса голосования
+        #     vote_granted = True  # Здесь ваша логика голосования
+        #     return RequestVoteResponse(term=request.term, vote_granted=vote_granted)
 
-    def run_server(self):
-        print(f"{self.name} запущен на порту {self.port}")
-        self.app.run(port=self.port, host='0.0.0.0')
-
-    async def send_message(self, receiver_url: str, message):
-        if self.nodes_sessions[receiver_url] is None:
-            self.nodes_sessions[receiver_url] = aiohttp.ClientSession()
-        session = self.nodes_sessions[receiver_url] 
-        headers = {"Content-Type": "application/octet-stream"}
+    @asynccontextmanager
+    async def lifespan(self, app: FastAPI):
+        print(f"Node {self.host}:{self.port} is starting...")
+        self.client = httpx.AsyncClient()
         try:
-            async with session.post(receiver_url, data=pickle.dumps(message), headers=headers) as response:
-                if response.status == 200:
-                    print(f"{self.name} успешно отправил: {message}")
-                else:
-                    print(f"Ошибка при отправке сообщения: {response.status}")
+            yield
+        finally:
+            print(f"Node {self.host}:{self.port} is shutting down...")
+            await self.client.aclose()
+            # TODO: добавить выкл будильника
+        
+    async def start(self):
+        config = uvicorn.Config(self.app, host=self.host, port=self.port, log_level="info")
+        server = uvicorn.Server(config)
+        await server.serve()
+        
+    async def send_message(self, receiver: str, message: BaseModel):
+        try:
+            response = await self.client.post(f"http://{receiver}/request-vote", json=message.model_dump())
+            print(f"Response from {receiver}: {response.json()}")
         except Exception as e:
-            print(f"Ошибка подключения: {e}")
-                
-    async def send_parallel_messages(self, receivers: list[str], message):
+            print(f"Failed to connect to {receiver}: {e}")
+                    
+    async def send_parallel_messages(self, receivers: list[str], message: BaseModel):
         tasks = []
         for receiver in receivers:
             task = asyncio.create_task(self.send_message(receiver, message))
             tasks.append(task)
         await asyncio.gather(*tasks)
-        
-    # Нужно вызвать в конце!
-    async def stop(self):
-        for session in self.nodes_sessions.values():
-            if session is not None:
-                await session.close()
-        self.election_timer.cancel()
-                
-    # Вызвать после становления лидером!
-    def refresh_next_match_index(self):
-        for key in self.nodes_sessions.keys():
-            self.next_index[key] = len(self.log)
-            self.match_index[key] = 0
-            
-    # TODO: убрать хардкод в конце функции!
-    def start_election(self):
-        print(f"{self.name}: election is starting!")
-        self.state = NodeState.CANDIDATE
-        self.current_term += 1
-        self.votes_count = 1
-        self.voted_for = self.name
-        self.election_timer.start()
-        asyncio.create_task(self.send_parallel_messages(self.nodes_sessions.keys(), RequestVote(self.current_term, self.name, len(self.log) - 1, 0)))
-    
-    def respond_to_RequestVote(self, request: RequestVote):
-        if (self.voted_for is not None) | (request.term < self.current_term) | (request.last_log_term < self.last_log_term):
-            vote = False
-        elif (request.last_log_term == self.last_log_term) & (request.last_log_index < (len(self.log) - 1)):
-            vote = False
-        else:
-            vote = True
-            self.election_timer.start() # сбрасываем таймер
-            self.current_term = request.term
-            self.voted_for = request.candidate_id
-        data = RequestVoteResponse(self.current_term, vote_granted=vote)
-        asyncio.create_task(self.send_message(request.candidate_id, data))
-        # print("HERERERERERER")
-        # asyncio.get_running_loop()
-        # print("asyncio.get_running_loop()")
-    
-    def receive_RequestVoteResponse(self, response: RequestVoteResponse):
-        if response.vote_granted == True:
-            self.votes_count += 1
-            if self.votes_count > (len(self.nodes_sessions) / 2):
-                print(f'I\'m {self.name} a new leader!')
