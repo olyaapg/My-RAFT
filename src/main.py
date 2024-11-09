9b31c6ea9d6af11908cfa24d6ac3bd6ec58a65d1
Olga <o.pogibelnaya@g.nsu.ru>
2024-11-09 16:10:40 +0700

    сделала универсальный файл запуска

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import asyncio
+import sys
 from node import Node
 
-async def start_nodes():
-    node1 = Node(5000, ["localhost:5001", "localhost:5002"])
-    node2 = Node(5001, ["localhost:5000", "localhost:5002"])
-    node3 = Node(5002, ["localhost:5000", "localhost:5001"])
-
-    await asyncio.gather(
-        node1.start(),
-        node2.start(),
-        node3.start()
-    )
+#py src\main.py 5000 localhost:5001 localhost:5002 localhost:5003
+async def main():
+    node = Node(int(args[0]), args[1:])
+    await node.start()
 
 if __name__ == "__main__":
-    asyncio.run(start_nodes())
+    args = sys.argv[1:]
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
